baacb8539bc7fac3c9fedfcde7044527770823eb
Ser9i02021 <78425749+Ser9i02021@users.noreply.github.com>
2025-02-25 17:39:37 -0300

    Update MallClientArrivalSimulator.py

--- MallClientArrivalSimulator.py
+++ MallClientArrivalSimulator.py
@@ -47,18 +47,37 @@ class MallClientArrivalSimulator:
 
         MorningAfternoonEvening = 0  # Period counter (to track morning, afternoon, evening periods).
 
-        # Loop through periods (morning, afternoon, evening) and generate arrivals.
-        for lambda_rate, period in zip(self.lambda_rates, self.periods): 
-            arrivalsFromPeriod = np.random.poisson(lambda_rate, period)  # Generate arrivals based on Poisson distribution.
-            
-            # Distribute client reservation prices and staying times using log-normal distributions.
-            rpDistribution = LogNormalReservationPricesDistribution(mu=None, sigma=None, sample_size=sum(arrivalsFromPeriod))
-            stDistribution = LogNormalStayingTimesDistribution(mu=None, sigma=None, sample_size=sum(arrivalsFromPeriod))
 
-            self.arrivals_per_time_slice.extend(arrivalsFromPeriod)  # Append the arrivals to the list.
+        # Generation of arrivals for each period (Morning, Afternoon and Evening)
+        for lambda_rate, period in zip(self.lambda_rates, self.periods): # Iteration over Morning, Afternoon and Evening
+            arrivalsFromPeriod = np.random.poisson(lambda_rate, period)  # Generate arrivals based on Poisson distribution. 
+                                                                         # E.g.: [4, 6, 7, 3, 2, 4, 8, 5]
+            print("AFP =", end=" ")
+            print(arrivalsFromPeriod)
+            print(len(arrivalsFromPeriod))
+            print()
+
+            self.arrivals_per_time_slice.extend(arrivalsFromPeriod)  # Append the arrivals from the current period to the list.
             
-            # Loop through each time slice in the current period (morning/afternoon/evening).
-            for i in range(1, len(arrivalsFromPeriod) + 1):
+        self.total_arrivals = sum(self.arrivals_per_time_slice) # Get the total arrivals for the day
+        # Distribute client reservation prices and staying times using log-normal distributions for all clients on the day
+        rpDistribution = LogNormalReservationPricesDistribution(mu=None, sigma=None, sample_size=sum(self.arrivals_per_time_slice))
+        stDistribution = LogNormalStayingTimesDistribution(mu=None, sigma=None, sample_size=sum(self.arrivals_per_time_slice))
+
+
+        acc = 0 # Accumulation variable for clients's ID generation and correct staying times and reservation prices attributions
+        for period in range(len(self.periods)): # Iteration over Morning, Afternoon and Evening
+            #arrivalsFromPeriod = np.random.poisson(lambda_rate, period)  # Generate arrivals based on Poisson distribution. 
+                                                                         # E.g.: [4, 6, 7, 3, 2, 4, 8, 5]
+            
+            #rpDistribution = LogNormalReservationPricesDistribution(mu=None, sigma=None, sample_size=sum(arrivalsFromPeriod))
+            #stDistribution = LogNormalStayingTimesDistribution(mu=None, sigma=None, sample_size=sum(arrivalsFromPeriod))
+
+            #self.arrivals_per_time_slice.extend(arrivalsFromPeriod)  # Append the arrivals from the current period to the list.
+            
+            # Loop through each time slice (hour) in the current period (morning/afternoon/evening).
+            # Index "i" refers to the current hour of the current period
+            for i in range(1, self.periods[period] + 1):
                 print(f"instant {self.simTime}")
 
                 # Check if any clients are ready to leave (free their parking slots).
@@ -70,32 +89,36 @@ class MallClientArrivalSimulator:
                         ci -= 1  # Adjust index after removal.
                     ci += 1
 
-                # For each time slice, predict the demand for that slice based on historical data.
-                pp.setPrice(self.demandHistory, MorningAfternoonEvening * len(arrivalsFromPeriod) + (i - 1))
+                # For each time slice, predict the demand for that slice (hour) based on historical data.
+                pp.setPrice(self.demandHistory, sum(self.periods[:MorningAfternoonEvening]) + (i - 1))
 
-                # Determine the range of clients arriving in this time slice.
-                initial = 0 if i == 1 else arrivalsFromPeriod[i - 2]
-                end = arrivalsFromPeriod[i - 1] if i == 1 else arrivalsFromPeriod[i - 1] + arrivalsFromPeriod[i - 2]
+                # Determine the range of clients arriving in this time slice (hour).
+                initial = acc
+                end = self.arrivals_per_time_slice[sum(self.periods[:MorningAfternoonEvening]) + (i - 1)] + acc
+                #end = arrivalsFromPeriod[i - 1] + acc
+                #initial = 0 if i == 1 else arrivalsFromPeriod[i - 2]
+                #end = arrivalsFromPeriod[i - 1] if i == 1 else arrivalsFromPeriod[i - 1] + arrivalsFromPeriod[i - 2]
 
-                # Handle client arrivals for the current time slice.
+                # Handle client arrivals for the current time slice (hour).
                 for clientNum in range(initial, end):
                     clientTimeStay = stDistribution.stayingTimes[clientNum]  # Time each client stays.
                     clientReservationPrice = rpDistribution.reservation_prices[clientNum]  # Client reservation price.
                     
                     # Create a new potential client.
-                    newPotentialClient = Client(self.total_arrivals + clientNum, clientReservationPrice, clientTimeStay, pp)
+                    newPotentialClient = Client(clientNum, clientReservationPrice, clientTimeStay, pp)
                     
                     # Try to occupy a parking slot. If successful, update the parking slot price and add the client to active list.
                     if newPotentialClient.tryOccupyParkingSlot(pp.getPrice(), self.simTime):
-                        pp.setPrice(self.demandHistory, MorningAfternoonEvening * len(arrivalsFromPeriod) + (i - 1))  # Update price after occupation.
+                        pp.setPrice(self.demandHistory, sum(self.periods[:MorningAfternoonEvening]) + (i - 1))  # Update price after occupation.
                         pp.clientsInPP.append(newPotentialClient)  # Add client to active client list.
                         print(f"client {newPotentialClient.id} occupied slot {newPotentialClient.pSlotOccupiedIndex}")
 
-                self.simTime += 1  # Increment the simulation time after each time slice.
+                self.simTime += 1  # Increment the simulation time after each time slice (hour).
+                acc = end
 
             MorningAfternoonEvening += 1  # Move to the next period (afternoon/evening).
-            self.total_arrivals += sum(arrivalsFromPeriod)  # Update the total arrivals count.
-        
+            #self.total_arrivals += sum(arrivalsFromPeriod)  # Update the total arrivals count with the arrivals from that period
+
     def plot_arrivals(self):
         """
         Plots the simulated client arrivals per hour.
